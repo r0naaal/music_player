ed2f6fbc4beefbcc37538534a40ee78ed045f920
Ronal <ronalandres74@gmail.com>
2025-06-19 19:37:28 -0500

    Initial commit

---
+++ src/main/java/com/example/MusicPlayerApp.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MusicPlayerApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.example.musicplayer {
+    requires javafx.controls;
+    requires javafx.media;
+    requires javafx.graphics;
+    exports com.example;
+}
